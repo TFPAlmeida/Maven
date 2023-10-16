1d07cd930943de6eb5afa9376c95cbfea6c22abf
Tiago Filipe Almeida <tfilipal@emeal.nttdata.com>
2023-10-16 10:33:27 +0100

    maven_3

---
+++ src/main/java/com/nttdata/financas/contaspagar/TesteApp.java
@@ -0,0 +1,15 @@
+package com.nttdata.financas.contaspagar;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class TesteApp {
+	
+	public static void main(String[] args) {
+		
+		String name = "Tiago Filipe Pereira de Almeida";
+		
+		System.out.println(StringUtils.abbreviate(name, 10));
+	
+	}
+	
+}
